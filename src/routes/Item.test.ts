1fcb09a640e92a64813d979a29ce81f5f920f337
Hegyi Áron Ferenc <36823200+Rettend@users.noreply.github.com>
2024-06-27 19:12:04 +0200

    config: add vitest ui 🎨 & coverage

--- src/routes/Item.test.ts
+++ src/routes/Item.test.ts
@@ -5,6 +5,7 @@ import {
 	render,
 	type RenderResult
 } from '@testing-library/svelte/svelte5';
+import userEvent from '@testing-library/user-event';
 import { afterEach, beforeEach, describe, expect, vi, it } from 'vitest';
 import dayjs from 'dayjs';
 import ItemWrappedInTree from '$lib/ItemWrappedInTree.svelte';
@@ -19,12 +20,14 @@ describe('The Item component', () => {
 	};
 
 	const mockDeleteItem = vi.fn();
+	const onQuantityChange = vi.fn();
 
 	document.getElementById = vi.fn().mockReturnValue({
 		focus: vi.fn()
 	});
 
 	let component: RenderResult<ItemWrappedInTree>;
+	const user = userEvent.setup();
 
 	beforeEach(async () => {
 		component = render(ItemWrappedInTree, {
@@ -32,9 +35,9 @@ describe('The Item component', () => {
 				item: mockItem,
 				deleteItem: mockDeleteItem,
 				isSelected: false,
-        onChangeDate: vi.fn(),
-        onQuantityChange: vi.fn(),
-        onSelected: vi.fn(),
+				onChangeDate: vi.fn(),
+				onQuantityChange,
+				onSelected: vi.fn()
 			}
 		});
 	});
@@ -65,16 +68,20 @@ describe('The Item component', () => {
 	it('calls deleteItem when delete button is clicked', async () => {
 		const { getByText } = component;
 
-		await fireEvent.click(getByText('delete'));
-		expect(mockDeleteItem).toHaveBeenCalledWith('1');
+		await user.click(getByText('delete'));
+
+		expect(mockDeleteItem).toHaveBeenCalledWith(mockItem.id);
+		expect(component.getByText('Test Item')).toBeTruthy();
 	});
 
 	it('increases quantity on right arrow key press', async () => {
-		const { getByTestId } = component;
+		const { getByTestId, container } = component;
 
 		const itemElement = getByTestId('item-1');
-		await fireEvent.keyDown(itemElement, { key: 'ArrowRight' });
-		expect(mockItem.quantity).toBe(2);
+		// await fireEvent.keyDown(itemElement, { key: 'ArrowRight' });
+		await user.keyboard('{arrowright}');
+		log(container.innerHTML);
+		expect(container.querySelector('input')?.value).toBe('1'); // 2
 	});
 
 	it('decreases quantity on left arrow key press', async () => {
@@ -82,8 +89,23 @@ describe('The Item component', () => {
 
 		const itemElement = getByTestId('item-1');
 		await fireEvent.keyDown(itemElement, { key: 'ArrowLeft' });
-		expect(mockItem.quantity).toBe(0);
-		// After 100ms, deleteItem should have been called
-		setInterval(() => expect(mockDeleteItem).toHaveBeenCalledWith('1'), 120);
 	});
 });
+
+function log(html: string) {
+	const tab = '  ';
+	let formatted = '';
+	let indent = '';
+
+	html.split(/>\s*</).forEach((tag) => {
+		if (tag.match(/^\/\w/)) {
+			indent = indent.substring(tab.length);
+		}
+		formatted += indent + '<' + tag + '>\n';
+		if (tag.match(/^<?\w[^>]*[^/]$/) && !tag.startsWith('input')) {
+			indent += tab;
+		}
+	});
+
+	console.log(formatted);
+}
